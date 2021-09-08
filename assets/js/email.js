203b444840bf171a7832941e53aff8e3bdfe947b
michaelangelopineda <pineda_michaelangelo@yahoo.com>
2021-09-08 21:04:38 +0000

    Add modal response when sending email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,19 +1,36 @@
-
-// from Code Institute
+// from Code Institute lesson on emailJS and Frozan website
 function sendMail(contactForm) {
     emailjs.send("service_xgft8wg", "lumiere", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "from_contact": contactForm.phone.value,
-        "message": contactForm.message.value,
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "from_contact": contactForm.phone.value,
+            "message": contactForm.message.value,
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                document.getElementById("contactForm").reset();
+                showModal();
+                document.getElementById("modalText").innerHTML =
+                    "Thank you for your message! <br> We will get back to you as soon as possible.";
+            },
+            function (error) {
+                console.log("FAILED", error);
+                showModal();
+                document.getElementById("modalText").innerHTML =
+                    "Something went wrong! Please try again.";
+            }
+        );
+    return false; // To block from loading a new page
 }
+
+/**
+ * Shows modal when submitting contact form and adds click function to modal button to be able to close modal.
+ */
+ function showModal() {
+    document.getElementById("modal-background").style.display = "block";
+}
+
+document.getElementById("modal-button").addEventListener("click", function () {
+    document.getElementById("modal-background").style.display = "none";
+});
